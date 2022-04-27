708b32b35784ce617f74541ed0d67afaca26b1ed
keyservlad <arnaud.guilhamat@isep.fr>
2022-04-27 19:51:27 -0400

    create user page styling

---
+++ pages/create-user.js
@@ -0,0 +1,71 @@
+export default function CreateUser() {
+  return (
+    <div className="container">
+      <div>
+        <div className="create-user">
+          <div className="create-user__top">
+            <div className="create-user__logo" />
+            <span className="create-user__title">Who is Watching?</span>
+          </div>
+          <div className="create-user__form">
+            <img
+              src="https://randomuser.me/api/portraits/men/32.jpg"
+              alt=""
+              className="create-user__user-img"
+            />
+            <div className="create-user__input-group">
+              <label>Name</label>
+              <input type="text" className="create-user__inputText" />
+              <div className="create-user__colors">
+                <div
+                  className="create-user__color create-user__color--active"
+                  style={{
+                    background: "rgb(2,0,36)",
+                    background:
+                      "linear-gradient(135deg, rgba(2,0,36,1) 11%, rgba(119,30,135,1) 100%)",
+                  }}
+                />
+                <div
+                  className="create-user__color"
+                  style={{
+                    background: "rgb(2,0,36)",
+                    background:
+                      "linear-gradient(135deg, rgba(2,0,36,1) 11%, rgba(44,135,30,1) 100%)",
+                  }}
+                />
+                <div
+                  className="create-user__color"
+                  style={{
+                    background: "rgb(2,0,36)",
+                    background:
+                      "linear-gradient(135deg, rgba(2,0,36,1) 11%, rgba(135,30,30,1) 100%)",
+                  }}
+                />
+                <div
+                  className="create-user__color"
+                  style={{
+                    background: "rgb(2,0,36)",
+                    background:
+                      "linear-gradient(135deg, rgba(2,0,36,1) 11%, rgba(30,41,135,1) 100%)",
+                  }}
+                />
+                <div
+                  className="create-user__color"
+                  style={{
+                    background: "rgb(2,0,36)",
+                    background:
+                      "linear-gradient(135deg, rgba(2,0,36,1) 11%, rgba(133,135,30,1) 100%)",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="create-user__buttons">
+            <button className="create-user__cancel">Cancel</button>
+            <button className="create-user__save">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
